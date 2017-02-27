8ded7c2919c1c03290ec1367d496a3d5e9ac294f
Murray McQuarrie <murray.mcquarrie@gmail.com>
2017-02-27 22:09:15 +0000

    added android layout

--- app/src/main/java/example/codeclan/com/stringsplit/SplitActivity.java
+++ app/src/main/java/example/codeclan/com/stringsplit/SplitActivity.java
@@ -2,12 +2,29 @@ package example.codeclan.com.stringsplit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class SplitActivity extends AppCompatActivity {
+
+    EditText wordsEditText;
+    Button runButton;
+    TextView countText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_split);
+        wordsEditText = (EditText) findViewById(R.id.words_edittext);
+        runButton = (Button) findViewById(R.id.run_btn);
+        countText = (TextView) findViewById(R.id.count_text);
+    }
+
+    public void onRunButtonClicked(View button) {
+        StringSplitter splitter = new StringSplitter();
+        int gotWords = splitter.countWords();
+        countText.setText(gotWords);
     }
 }
---
+++ app/src/test/java/example/codeclan/com/stringsplit/StringSplitterTest.java
@@ -0,0 +1,26 @@
+package example.codeclan.com.stringsplit;
+    import org.junit.Before;
+    import org.junit.Test;
+    import static org.junit.Assert.*;
+    import java.util.ArrayList;
+
+/**
+ * Created by user on 27/02/2017.
+ */
+
+public class StringSplitterTest {
+
+    ArrayList<String> testCounted;
+
+    @Before
+    public void before() {
+        testCounted = new ArrayList<String>();
+        testCounted.add();
+    }
+
+    @Test
+    public void getCount() {
+
+    }
+}
+}
